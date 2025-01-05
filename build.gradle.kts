3cd135b7b3191e594b5f49b0feb58fe7e3a2565e
LetsWorkAround <peene_dev@naver.com>
2025-01-06 01:33:27 +0900

    v1.0.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library") version "8.2.0"
+    id("com.android.library") version "8.3.2"
     id("org.jetbrains.kotlin.android") version "1.9.22"
     id("maven-publish")
 }
@@ -55,9 +55,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-
-//    implementation(project(":commonDatas"))
-    implementation("com.github.LetsWorkAround:commonDatas:v1.0.7")  // 최신 release 버전
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
